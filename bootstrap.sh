2d9b97c29fb15c042bfba94e9aa25e5f0d6975b7
neallred <neallred@gmail.com>
2020-01-02 19:14:16 -0600

    better bootstrapping

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+command -v rustc || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source ~/.profile
+command -v wasm-pack || curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+if [ -e data-bundler/data/words-index.json.gz ]
+then
+    echo "found indices, skipping index creation"
+else
+  cargo run --release --bin data-bundler
+fi
+wasm-pack build --release client/
+pushd client/web/
+npm i
+npm run build
+popd
+cargo build --release --bin server
